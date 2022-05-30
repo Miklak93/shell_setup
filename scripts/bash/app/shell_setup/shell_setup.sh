894c0c55d0544c0931ce71b6bf7b0dbfcadfd7c9
Mikołaj <m.stefanski93@wp.pl>
2022-05-30 14:08:43 +0200

    Inputrc introduced

--- scripts/bash/app/shell_setup/shell_setup.sh
+++ scripts/bash/app/shell_setup/shell_setup.sh
@@ -4,6 +4,8 @@
 ####################################FUNCTIONS####################################
 rm -f /home/$USER/.bashrc
 rm -rf /home/$USER/.vim
+rm -rf /home/$USER/.inputrc
 ln -s /repo/$USER/shell_setup/vim/.vim /home/$USER/.vim
 ln -s /repo/$USER/shell_setup/vim/.vimrc /home/$USER/.vimrc
 ln -s /repo/$USER/shell_setup/bash/.bashrc /home/$USER/.bashrc
+ln -s /repo/$USER/shell_setup/bash/.inputrc /home/$USER/.inputrc
