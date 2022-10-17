f1e7bb1ebd9e8cde62f3bea6fe4a1ebea9410759
Mikłak <87863260+Miklak93@users.noreply.github.com>
2022-10-17 15:27:58 +0200

    Update shell_setup.sh

--- scripts/bash/app/shell_setup/shell_setup.sh
+++ scripts/bash/app/shell_setup/shell_setup.sh
@@ -4,6 +4,7 @@
 ####################################FUNCTIONS####################################
 rm -f /home/$USER/.bashrc
 rm -rf /home/$USER/.vim
+rm -rf /home/$USER/.vimrc
 rm -rf /home/$USER/.inputrc
 ln -s /repo/$USER/shell_setup/vim/.vim /home/$USER/.vim
 ln -s /repo/$USER/shell_setup/vim/.vimrc /home/$USER/.vimrc
