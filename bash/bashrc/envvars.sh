2c37ed9b697f6e4814b670bc8c523642228c500f
Mikolaj Stefanski <mikolaj.stefanski@sabre.com>
2023-01-01 23:50:28 +0100

    envvars corrected

--- bash/bashrc/envvars.sh
+++ bash/bashrc/envvars.sh
@@ -1,4 +1,5 @@
-BASH_AUTOCOMPLETION_PATH="/repo/$USER/shell_setup/scripts/bash/autocompletion"
-BASH_SCRIPTS_PATH="/repo/$USER/shell_setup/scripts/bash/app"
+REPO_PATH="/repo/$USER"
+BASH_AUTOCOMPLETION_PATH="$REPO_PATH/shell_setup/scripts/bash/autocompletion"
+BASH_SCRIPTS_PATH="$REPO_PATH/shell_setup/scripts/bash/app"
 
 export PATH=$BASH_AUTOCOMPLETION_PATH:$BASH_SCRIPTS_PATH:$PATH
